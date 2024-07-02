18f387ffb19f9f35b3d835388566cee5ad155e15
Pam Selle <pamela.selle@gmail.com>
2024-07-02 11:41:29 -0400

    Formatting & breaking params into object

--- hooks/useSubscription.ts
+++ hooks/useSubscription.ts
@@ -12,7 +12,10 @@ type PagingKey = string
  * Paging tokens for each contract/topic pair. These can be mutated directly,
  * rather than being stored as state within the React hook.
  */
-const paging: Record<PagingKey, { lastLedgerStart?: number, pagingToken?: string }> = {}
+const paging: Record<
+  PagingKey,
+  { lastLedgerStart?: number; pagingToken?: string }
+> = {}
 
 /**
  * Subscribe to events for a given topic from a given contract, using a library
@@ -39,50 +42,54 @@ export function useSubscription(
     async function pollEvents(): Promise<void> {
       try {
         if (!paging[id].lastLedgerStart) {
-          let latestLedgerState = await server.getLatestLedger();
+          const latestLedgerState = await server.getLatestLedger()
           paging[id].lastLedgerStart = latestLedgerState.sequence
-        } 
-       
-        let response = await server.getEvents({
-          startLedger: !paging[id].pagingToken
-            ? paging[id].lastLedgerStart
-            : undefined,
+        }
+        const startLedger = !paging[id].pagingToken
+          ? paging[id].lastLedgerStart
+          : undefined
+
+        const params: SorobanClient.Server.GetEventsRequest = {
+          startLedger,
           cursor: paging[id].pagingToken,
           filters: [
             {
               contractIds: [contractId],
-              topics: [[
-                xdr.ScVal.scvSymbol(topic).toXDR("base64")
-              ]],
-              type: "contract"
-            }  
+              topics: [[xdr.ScVal.scvSymbol(topic).toXDR('base64')]],
+              type: 'contract',
+            },
           ],
-          limit: 10
-        });
-     
-        paging[id].pagingToken = undefined;
-        if (response.latestLedger) {
-          paging[id].lastLedgerStart = parseInt(response.latestLedger);
+          limit: 10,
         }
-        response.events && response.events.forEach(event => {
-          try {
-            onEvent(event)
-          } catch (error) {
-            console.error("Poll Events: subscription callback had error: ", error);
-          } finally {
-            paging[id].pagingToken = event.pagingToken
-          }
-        }) 
+        let response = await server.getEvents(params)
+
+        paging[id].pagingToken = undefined
+        if (response.latestLedger) {
+          paging[id].lastLedgerStart = parseInt(response.latestLedger)
+        }
+        response.events &&
+          response.events.forEach(event => {
+            try {
+              onEvent(event)
+            } catch (error) {
+              console.error(
+                'Poll Events: subscription callback had error: ',
+                error
+              )
+            } finally {
+              paging[id].pagingToken = event.pagingToken
+            }
+          })
       } catch (error) {
-        console.error("Poll Events: error: ", error);
+        console.error('Poll Events: error: ', error)
       } finally {
         if (!stop) {
-          timeoutId = setTimeout(pollEvents, pollInterval);
+          timeoutId = setTimeout(pollEvents, pollInterval)
         }
       }
     }
 
-    pollEvents();
+    pollEvents()
 
     return () => {
       if (timeoutId != null) clearTimeout(timeoutId)
